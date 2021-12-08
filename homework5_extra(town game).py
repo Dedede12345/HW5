8b3a914d87e9ea2546a783106a7f10f89e64e482
Geb Den <gleb@adenisov.com>
2021-12-08 19:38:26 +0300

    town game start

---
+++ homework5_extra(town game).py
@@ -0,0 +1,23 @@
+answer = 'Минск'
+
+end = answer[-1]
+
+print(f'Начальный город: {answer}')
+
+forbidden = ['й', 'ь', 'ъ']
+
+while True:
+    answer = input(f'Введите город начинающийся с \'{end}\' ').casefold()
+
+    if answer == 'сдаюсь':
+        break
+
+    for i in forbidden:
+        answer = answer.replace(i, '')
+    if answer[0] == end:
+        end = answer[-1]
+        continue
+    else:
+        print('Попрубуйте ввести еще раз или введите \'сдаюсь\'\n (С любым регистром!!!)')
+
+print('Спасибо за игру!')
